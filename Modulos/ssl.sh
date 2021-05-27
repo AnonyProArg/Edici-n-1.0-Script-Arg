f770dc29da4802a7709c4cdb3987020f19e9c19d
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2021-05-27 20:32:49 -0300

    Update ssl.sh

--- Modulos/ssl.sh
+++ Modulos/ssl.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#25/01/2021 by @Kalix1
+#25/01/2021 by AnonyProArg
 clear
 clear
-SCPdir="/etc/VPS-MX"
+SCPdir="/etc/VPS-ARG"
 SCPfrm="${SCPdir}/herramientas" && [[ ! -d ${SCPfrm} ]] && exit
 SCPinst="${SCPdir}/protocolos"&& [[ ! -d ${SCPinst} ]] && exit
 declare -A cor=( [0]="\033[1;37m" [1]="\033[1;34m" [2]="\033[1;31m" [3]="\033[1;33m" [4]="\033[1;32m" )
@@ -50,7 +50,7 @@ echo -e "\033[1;33m $(fun_trans  "Detenido Con Exito!")"
 msg -bar
 return 0
 }
-echo -e "\033[1;32m $(fun_trans  "              INSTALADOR SSL By VPS-MX")"
+echo -e "\033[1;32m $(fun_trans  "              INSTALADOR SSL By VPS-ARG")"
 msg -bar
 echo -e "\033[1;33m $(fun_trans  "Seleccione una puerta de redirección interna.")"
 echo -e "\033[1;33m $(fun_trans  "Un puerto SSH/DROPBEAR/SQUID/OPENVPN/SSL")"
@@ -86,7 +86,7 @@ echo -e "client = no\n[SSL]\ncert = /etc/stunnel/stunnel.pem\naccept = ${SSLPORT
 ####Coreccion2.0##### 
 openssl genrsa -out stunnel.key 2048 > /dev/null 2>&1
 
-(echo "mx" ; echo "mx" ; echo "mx" ; echo "mx" ; echo "mx" ; echo "mx" ; echo "@vpsmx" )|openssl req -new -key stunnel.key -x509 -days 1000 -out stunnel.crt > /dev/null 2>&1
+(echo "mx" ; echo "mx" ; echo "mx" ; echo "mx" ; echo "mx" ; echo "mx" ; echo "@vpsarg" )|openssl req -new -key stunnel.key -x509 -days 1000 -out stunnel.crt > /dev/null 2>&1
 
 cat stunnel.crt stunnel.key > stunnel.pem 
 
@@ -153,7 +153,7 @@ clear
 msg -bar
 msg -bar3
 msg -tit
-echo -e "${cor[3]}       INSTALADOR MONO Y MULTI SSL By @Kalix1"
+echo -e "${cor[3]}       INSTALADOR MONO Y MULTI SSL By AnonyProArg"
 msg -bar
 echo -e "${cor[1]}            Escoja la opcion deseada."
 msg -bar
