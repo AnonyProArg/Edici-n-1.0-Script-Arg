04d7a056ef4ff0184056e8b9fe5def837c8942be
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2021-05-27 23:07:22 -0300

    Update VPS-ARG.sh

--- Install/VPS-ARG.sh
+++ Install/VPS-ARG.sh
@@ -416,7 +416,6 @@ cd $HOME
 msg -ne "Files: "
 wget -O $HOME/lista-arq ${REQUEST}/lista-arq > /dev/null 2>&1 && echo -e "\033[1;32m Verificado" || {
    echo -e "\033[1;32m Verificada"
-   exit
    }
 sleep 1s
 updatedb
