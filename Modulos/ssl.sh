6033ca8958c3d72ecd51f41df7aa9dde2797c338
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2021-05-28 22:45:47 -0300

    Update ssl.sh

--- Modulos/ssl.sh
+++ Modulos/ssl.sh
@@ -150,6 +150,47 @@ rm -rf /root/stunnel.key > /dev/null 2>&1
 return 0
 }
 clear
+ssl_multi () {
+echo -e ""
+printf "${RED}INSTANDO PYTHON DIRECT MODIFICADO..."
+echo -e ""
+printf "${NC}+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
+screen -dmS Pydirect python /etc/VPS-ARG/protocolos/PDirect.py 80
+printf "${RED}Se instalará SSL en puerto 443 redirecionado a python 80"
+echo -e ""
+printf "${NC}+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
+echo -e ""
+printf "${RED}Instalando SSL"
+echo -e ""
+printf "${NC}+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
+apt-get install stunnel4 -y
+apt-get install stunnel4 -y > /dev/null 2>&1
+echo -e "client = no\n[SSL]\ncert = /etc/stunnel/stunnel.pem\naccept = 443\nconnect = 127.0.0.1:80" > /etc/stunnel/stunnel.conf
+openssl genrsa -out stunnel.key 2048 > /dev/null 2>&1
+
+(echo "US" ; echo "California" ; echo "San Francisco" ; echo "Cloudflare, inc." ; echo "" ; echo "sni.cloudflaressl.com" ; echo "" )|openssl req -new -key stunnel.key -x509 -days 1000 -out stunnel.crt > /dev/null 2>&1
+
+cat stunnel.crt stunnel.key > stunnel.pem 
+
+mv stunnel.pem /etc/stunnel/
+######-------
+sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
+service stunnel4 restart > /dev/null 2>&1
+printf "${NC}+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
+echo -e ""
+printf "${RED}INSTALADO STUNNEL4 CON EXITO"
+echo -e ""
+printf "${NC}+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
+rm -rf /etc/ger-frm/stunnel.crt > /dev/null 2>&1
+rm -rf /etc/ger-frm/stunnel.key > /dev/null 2>&1
+rm -rf /root/stunnel.crt > /dev/null 2>&1
+rm -rf /root/stunnel.key > /dev/null 2>&1
+printf "${NC}+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
+echo -e ""
+printf "${RED}INSTALACION FINALIZADA"
+return 0
+}
+clear
 msg -bar
 msg -bar3
 msg -tit
@@ -159,7 +200,7 @@ echo -e "${cor[1]}            Escoja la opcion deseada."
 msg -bar
 echo -e "${cor[4]} 1).-\033[1;37m ININICIAR O PARAR SSL "
 echo -e "${cor[4]} 2).-\033[1;37m AGREGAR PUERTOS SSL   "
-"multissl.sh")echo -ne "$(msg -verd " [$2]") $(msg -verm2 "==>>") " && msg -azu "PDirect + SSL(AUTOMATICO)$(pid_inst multissl)";;
+echo -e "${cor[4]} 3).-\033[1;37m AGREGAR PUERTOS SSL   "
 msg -bar
 echo -ne "\033[1;37mDigite solo el numero segun su respuesta: "
 read opcao
@@ -173,5 +214,9 @@ msg -bar
 ssl_stunel_2
 sleep 3
 exit
+;;
+3)
+msg -bar
+ssl_multi
 ;;
 esac
