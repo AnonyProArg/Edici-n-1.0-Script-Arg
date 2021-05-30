9c7f3de1f48df1df0a3b07de02291f8e9000b51f
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2021-05-30 19:21:21 -0300

    Update actualizador.sh

--- Modulos/actualizador.sh
+++ Modulos/actualizador.sh
@@ -3,6 +3,13 @@ clear
 msg -bar
 msg -tit
 echo -e "\033[1;91m      ACTULIZAR FICHEROS DEL SCRIPT VPS-ARG"
-echo -e "\e[032m               NO HAY NADA NUEVO AUN"
+echo -e "\e[032m               Actualizando"
 echo -e ""
+echo -e "\e[032m   1) Incluye xray"
+wget https://raw.githubusercontent.com/AnonyProArg/Edici-n-1.0-Script-Arg/main/Modulos/menu -O /etc/VPS-ARG/menu
+wget https://raw.githubusercontent.com/AnonyProArg/Xray/main/xray.sh -O /etc/VPS-ARG/Xray.sh
+git clone https://github.com/AnonyProArg/Xray.git /etc/VPS-ARG/
+chmod -R 777 /etc/VPS-ARG
+echo -e ""
+
 
