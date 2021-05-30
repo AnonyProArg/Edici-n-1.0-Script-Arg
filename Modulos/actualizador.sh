195907077417bd38c601046b60344b9d9e7aa7d2
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2021-05-30 18:17:43 -0300

    Update actualizador.sh

--- Modulos/actualizador.sh
+++ Modulos/actualizador.sh
@@ -1,4 +1,7 @@
+clear
+clear
 msg -bar
 msg -tit
-echo -e "\033[1;93m      NO HAY NADA NUEVO AUN"
+echo -e "\033[1;91m      ACTULIZAR FICHEROS DEL SCRIPT VPS-ARG"
+echo -e "${cor[2]}     NO HAY NADA NUEVO AUN"
 echo -e ""
