6b4708f43da50e15957b8aa5a084c82de5ada604
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2021-05-31 00:14:53 -0300

    Update actualizador.sh

--- Modulos/actualizador.sh
+++ Modulos/actualizador.sh
@@ -1,10 +1,28 @@
-wget https://raw.githubusercontent.com/AnonyProArg/Edici-n-1.0-Script-Arg/main/Modulos/menu -O /etc/VPS-ARG/menu
-wget https://raw.githubusercontent.com/AnonyProArg/Xray/main/xray.sh -O /etc/VPS-ARG/protocolos/xray.sh
-git clone https://github.com/AnonyProArg/Xray.git /etc/VPS-ARG/Xray
-git clone https://github.com/AnonyProArg/Sms-Spam.git /etc/VPS-ARG/Sms-Spam
-wget https://raw.githubusercontent.com/AnonyProArg/Edici-n-1.0-Script-Arg/main/Install/Version -O /etc/versin_script
-wget https://raw.githubusercontent.com/AnonyProArg/Sms-Spam/main/SMS.sh -O /etc/VPS-ARG/herramientas/SMS.sh
-wget https://raw.githubusercontent.com/AnonyProArg/Sms-Spam/main/.notify -O /etc/VPS-ARG/Sms-Spam/.notify
+fun_bar () {
+comando="$1"
+ _=$(
+$comando > /dev/null 2>&1
+) & > /dev/null
+pid=$!
+while [[ -d /proc/$pid ]]; do
+echo -ne "  \033[1;33m["
+   for((i=0; i<40; i++)); do
+   echo -ne "\033[1;31m>"
+   sleep 0.1
+   done
+echo -ne "\033[1;33m]"
+sleep 1s
+echo
+tput cuu1 && tput dl1
+done
+echo -ne "  \033[1;33m[\033[1;31m>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\033[1;33m] - \033[1;32m OK \033[0m\n"
+sleep 1s
+}
+##PAKETES
+echo ""
+echo -e "\033[97m    ◽️ INTENTANDO DETENER UPDATER SECUNDARIO ⏹️" 
+fun_bar " killall apt apt-get > /dev/null 2>&1 "
+
 chmod -R 777 /etc/VPS-ARG
 clear
 clear
