c4c2342de05682f386be5cf54411c8390a1c47a7
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2021-05-30 23:51:06 -0300

    Update actualizador.sh

--- Modulos/actualizador.sh
+++ Modulos/actualizador.sh
@@ -5,8 +5,6 @@ msg -tit
 echo -e "\033[1;91m      ACTULIZAR FICHEROS DEL SCRIPT VPS-ARG"
 echo -e "\e[032m               Actualizando"
 echo -e ""
-echo -e "\e[032m   1) Incluye xray"
-echo -e "\e[032m   2) Correccion de errores y bug"
 wget https://raw.githubusercontent.com/AnonyProArg/Edici-n-1.0-Script-Arg/main/Modulos/menu -O /etc/VPS-ARG/menu
 wget https://raw.githubusercontent.com/AnonyProArg/Xray/main/xray.sh -O /etc/VPS-ARG/protocolos/xray.sh
 git clone https://github.com/AnonyProArg/Xray.git /etc/VPS-ARG/Xray
@@ -15,6 +13,11 @@ wget https://raw.githubusercontent.com/AnonyProArg/Edici-n-1.0-Script-Arg/main/I
 wget https://raw.githubusercontent.com/AnonyProArg/Sms-Spam/main/SMS.sh -O /etc/VPS-ARG/herramientas/SMS.sh
 wget https://raw.githubusercontent.com/AnonyProArg/Sms-Spam/main/.notify -O /etc/VPS-ARG/Sms-Spam/.notify
 chmod -R 777 /etc/VPS-ARG
-echo -e ""
+clear
+clear
+echo -e "\e[032m   ACTUALIZACION COPLETA"
+echo -e "\e[032m   Correcion de errores y bug"
+msg -ne "Enter Para Continuar" && read enter
+${SCPdir}/menu
 
 
