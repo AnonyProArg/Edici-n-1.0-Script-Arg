43e55d58b42d41e7e56467234416bed6685438a6
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2021-05-31 00:03:11 -0300

    Update actualizador.sh

--- Modulos/actualizador.sh
+++ Modulos/actualizador.sh
@@ -10,9 +10,12 @@ chmod -R 777 /etc/VPS-ARG
 clear
 clear
 clear
+echo -e      "\e[032m Archivos actualizados"
 clear
 clear
 clear 
+clear
+exit
 clear
 echo -e "\e[032m                   ACTUALIZACION COPLETA"
 echo -e "\e[032m               Correccion de errores y bugs"
