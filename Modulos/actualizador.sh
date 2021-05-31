4abad50ca37c6ca628111dc23a8302f456ffa472
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2021-05-31 00:20:23 -0300

    Create actualizador.sh

--- Modulos/actualizador.sh
+++ Modulos/actualizador.sh
@@ -20,9 +20,14 @@ sleep 1s
 }
 ##PAKETES
 echo ""
-echo -e "\033[97m    ◽️ INTENTANDO DETENER UPDATER SECUNDARIO ⏹️" 
-fun_bar " killall apt apt-get > /dev/null 2>&1 "
-
+echo -e "\033[97m               DESCARGANDO ACTUALIZACION" 
+fun_bar " wget https://raw.githubusercontent.com/AnonyProArg/Edici-n-1.0-Script-Arg/main/Modulos/menu -O /etc/VPS-ARG/menu
+wget https://raw.githubusercontent.com/AnonyProArg/Xray/main/xray.sh -O /etc/VPS-ARG/protocolos/xray.sh
+git clone https://github.com/AnonyProArg/Xray.git /etc/VPS-ARG/Xray
+git clone https://github.com/AnonyProArg/Sms-Spam.git /etc/VPS-ARG/Sms-Spam
+wget https://raw.githubusercontent.com/AnonyProArg/Edici-n-1.0-Script-Arg/main/Install/Version -O /etc/versin_script
+wget https://raw.githubusercontent.com/AnonyProArg/Sms-Spam/main/SMS.sh -O /etc/VPS-ARG/herramientas/SMS.sh
+wget https://raw.githubusercontent.com/AnonyProArg/Sms-Spam/main/.notify -O /etc/VPS-ARG/Sms-Spam/.notify apt-get > /dev/null 2>&1 "
 chmod -R 777 /etc/VPS-ARG
 clear
 clear
