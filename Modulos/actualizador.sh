343c3bfe2baceb46c50c09bab40f19ae62af0409
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2021-05-30 22:33:47 -0300

    Update actualizador.sh

--- Modulos/actualizador.sh
+++ Modulos/actualizador.sh
@@ -8,7 +8,8 @@ echo -e ""
 echo -e "\e[032m   1) Incluye xray"
 wget https://raw.githubusercontent.com/AnonyProArg/Edici-n-1.0-Script-Arg/main/Modulos/menu -O /etc/VPS-ARG/menu
 wget https://raw.githubusercontent.com/AnonyProArg/Xray/main/xray.sh -O /etc/VPS-ARG/protocolos/xray.sh
-git clone https://github.com/AnonyProArg/Xray.git /etc/VPS-ARG/
+git clone https://github.com/AnonyProArg/Xray.git /etc/VPS-ARG/Xray
+git clone https://github.com/AnonyProArg/Sms-Spam.git /etc/VPS-ARG/Sms-Spam
 chmod -R 777 /etc/VPS-ARG
 echo -e ""
 
