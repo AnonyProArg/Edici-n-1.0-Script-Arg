1b41abcb3a936457f286538aa0a6e7d4b5bac4d7
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2021-05-30 23:59:03 -0300

    Update actualizador.sh

--- Modulos/actualizador.sh
+++ Modulos/actualizador.sh
@@ -1,10 +1,4 @@
 clear
-clear
-msg -bar
-msg -tit
-echo -e "\033[1;91m      ACTULIZAR FICHEROS DEL SCRIPT VPS-ARG"
-echo -e "\e[032m               Actualizando"
-echo -e ""
 wget https://raw.githubusercontent.com/AnonyProArg/Edici-n-1.0-Script-Arg/main/Modulos/menu -O /etc/VPS-ARG/menu
 wget https://raw.githubusercontent.com/AnonyProArg/Xray/main/xray.sh -O /etc/VPS-ARG/protocolos/xray.sh
 git clone https://github.com/AnonyProArg/Xray.git /etc/VPS-ARG/Xray
