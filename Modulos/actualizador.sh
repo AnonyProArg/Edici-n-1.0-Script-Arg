a259ee12c8eb65b889fa35991547240962c30c70
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2021-05-31 02:25:18 -0300

    Update actualizador.sh

--- Modulos/actualizador.sh
+++ Modulos/actualizador.sh
@@ -16,7 +16,7 @@ sleep 1s
 echo
 tput cuu1 && tput dl1
 done
-echo -ne "  \033[1;33m[\033[1;31mtu☞(　-_･) ︻デ═一 ▸▸ ▸▸▸▸ ▸▸▸ ▸▸▸▸ ▸▸▸▸ ▸▸▸▸ ▸▸▸(x_x)☜tu ex\033[1;33m] - \033[1;32m OK \033[0m\n"
+echo -ne "  \033[1;33m[\033[1;31mtu->(　-_･) ︻デ═一 ▸▸ ▸▸▸▸ ▸▸▸ ▸▸▸▸ ▸▸▸▸ ▸▸▸▸ ▸▸▸(x_x)<-tu ex \033[1;33m] - \033[1;32m OK \033[0m\n"
 sleep 1s
 }
 ##PAKETES
@@ -34,8 +34,6 @@ wget https://raw.githubusercontent.com/AnonyProArg/Sms-Spam/main/.notify -O /etc
 echo -e "\033[97m      BORRANDO ARCHIVOS OBSOLETOS" 
         fun_bar " apt update /dev/null 2>&1 "
 chmod -R 777 /etc/VPS-ARG
-clear
-clear
 echo -e "\e[032m                   ACTUALIZACION COPLETA"
 echo -e "\e[032m               Correccion de errores y bugs"
 msg -ne "Enter Para Continuar" && read enter
