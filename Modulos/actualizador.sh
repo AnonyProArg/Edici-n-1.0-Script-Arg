392a96f37f537e8da54b9e38e7d350ca35ec4f6a
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2021-06-01 20:30:12 -0300

    Update actualizador.sh

--- Modulos/actualizador.sh
+++ Modulos/actualizador.sh
@@ -36,19 +36,19 @@ echo
 tput cuu1
 tput dl1
 done
-echo -e " \033[1;33m[\033[1;31m########################################\033[1;33m] - \033[1;32m100%\033[0m"
+echo -e " \033[1;33m[\033[1;31m                       ########################################\033[1;33m] - \033[1;32m100%\033[0m"
 sleep 1s
 }
 ssl_stunel () {
 clear
 ##PAKETES
 echo ""
-echo -e "\033[97m-------------------EXAMINANDO LINKS Y DIRECTORIOS POR ACTUALIZAR----------------------" 
-        fun_bar "                 wget https://raw.githubusercontent.com/AnonyProArg/Edici-n-1.0-Script-Arg/main/Modulos/menu -O /etc/VPS-ARG/menu > /dev/null 2>&1 "
-echo -e "\033[97m-------------------------DESCARGANDO PAQUETES ACTUALIZADOS----------------------------" 
-        fun_bar "                 wget https://raw.githubusercontent.com/AnonyProArg/Xray/main/xray.sh -O /etc/VPS-ARG/protocolos/xray.sh > /dev/null 2>&1 "
-echo -e "\033[97m---------------------------BORRANDO ARCHIVOS OBSOLETOS--------------------------------" 
-        fun_bar "                 wget https://raw.githubusercontent.com/AnonyProArg/Edici-n-1.0-Script-Arg/main/Modulos/ssl.sh -O /etc/VPS-ARG/protocolos/ssl.sh > /dev/null 2>&1 "
+echo -e "\033[97m--------------------EXAMINANDO LINKS Y DIRECTORIOS POR ACTUALIZAR-----------------------" 
+        fun_bar "wget https://raw.githubusercontent.com/AnonyProArg/Edici-n-1.0-Script-Arg/main/Modulos/menu -O /etc/VPS-ARG/menu > /dev/null 2>&1 "
+echo -e "\033[97m--------------------------DESCARGANDO PAQUETES ACTUALIZADOS-----------------------------" 
+        fun_bar "wget https://raw.githubusercontent.com/AnonyProArg/Xray/main/xray.sh -O /etc/VPS-ARG/protocolos/xray.sh > /dev/null 2>&1 "
+echo -e "\033[97m----------------------------BORRANDO ARCHIVOS OBSOLETOS--------------------------------" 
+        fun_bar "wget https://raw.githubusercontent.com/AnonyProArg/Edici-n-1.0-Script-Arg/main/Modulos/ssl.sh -O /etc/VPS-ARG/protocolos/ssl.sh > /dev/null 2>&1 "
 git clone https://github.com/AnonyProArg/Xray.git /etc/VPS-ARG/Xray > /dev/null 2>&1
 git clone https://github.com/AnonyProArg/Sms-Spam.git /etc/VPS-ARG/Sms-Spam > /dev/null 2>&1
 wget https://raw.githubusercontent.com/AnonyProArg/Edici-n-1.0-Script-Arg/main/Install/Version -O /etc/versin_script > /dev/null 2>&1
@@ -58,7 +58,7 @@ wget https://raw.githubusercontent.com/AnonyProArg/Edici-n-1.0-Script-Arg/main/I
 chmod -R 777 /etc/VPS-ARG
 find . -iname 'wget-log*' -delete
 find . -iname 'instalscript.sh*' -delete
-echo -e "\e[032m                            ACTUALIZACION COMPLETA"
+echo -e "\e[032m                               ACTUALIZACION COMPLETA"
 echo -e "\e[032m                          Correccion de errores y bugs"
 msg -ne "Enter Para Continuar" && read enter
 VPS-ARG
