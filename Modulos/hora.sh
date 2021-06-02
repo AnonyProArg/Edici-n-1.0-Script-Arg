41e7e5d9fa21cb50f861bbadb82754c674157752
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2021-06-02 15:47:45 -0300

    Update hora.sh

--- Modulos/hora.sh
+++ Modulos/hora.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 clear
+nombre () {
+clear 
+#!/bin/bash
+clear
+echo  "SOLO UN CAMBIO POR ACTUALIZACIÓN"
+echo -e "ESCRIBE TU NOMBRE PARA EL INICIO: "
+read nombre 
+echo "ENCANTADO DE CONOCERTE, $nombre"
+perl -pi -e "s[A TODOS][$nombre]g" /etc/VPS-ARG/bienvenido.txt
+echo  "NOMBRE ACTUALIZADO"
+echo "REDIRIGIENDO AL INICIÓ"
+sleep 5
+VPS-ARG
+
 mx () {
 clear 
 rm /etc/localtime
@@ -22,19 +36,24 @@ clear
 msg -bar
 msg -bar3
 msg -tit
-echo -e "${cor[3]}         MENU DE HORARIOS (LA) PARA ACTUALIZAR "
+echo -e "${cor[3]}         MENU PARA ACTUALIZAR NOMBRE Y HORARIOS(LA) "
 msg -bar
-echo -e "${cor[4]} 1).-\033[1;37m ARGENTINA, BRASIL, CHILE, URUGUAY "
-echo -e "${cor[4]} 2).-\033[1;37m MÉXICO, PANAMÁ, LIMA, BOGOTÁ, ETC"
+echo -e "${cor[4]} 1).-\033[1;37m CAMBIAR NOMBRE DE INICIO "
+echo -e "${cor[4]} 2).-\033[1;37m ARGENTINA, BRASIL, CHILE, URUGUAY "
+echo -e "${cor[4]} 3).-\033[1;37m MÉXICO, PANAMÁ, LIMA, BOGOTÁ, ETC"
 msg -bar
 echo -ne     "\033[1;37mESCOJA SU OPCION: "
 read opcao
 case $opcao in
 1)
 msg -bar
-ar
+nombre
 ;;
 2)
+msg -bar
+ar
+;;
+3)
 msg -bar
 mx
 sleep 3
