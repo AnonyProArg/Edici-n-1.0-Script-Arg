fe5e65acc024598d045ec3039741bdb37b64891c
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2021-06-02 20:22:14 -0300

    Update sh.sh

--- Modulos/sh.sh
+++ Modulos/sh.sh
@@ -1,9 +1,11 @@
-*************************************************
 #!/bin/bash
 PASS="123"
 read -s -p "Password: " mypassword
 echo ""
-
-if [ "$mypassword" == "$PASS" ] then killall konsole echo "Access Denied" else echo "Password accepted"| VPS-ARG fi
+if [ "$mypassword" == "$PASS" ] 
+then killall konsole echo "Access Denied" 
+else echo "Password accepted"
+VPS-ARG 
+fi
 
 *************************************************
