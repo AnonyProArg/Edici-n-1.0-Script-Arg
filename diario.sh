922958076e33b6d8524250360de20b999baaad14
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2021-06-02 12:52:50 -0300

    Update diario.sh

--- diario.sh
+++ diario.sh
@@ -19,7 +19,7 @@ echo -e "$portas"
 msg -bar
 msg -bar3
 msg -tit
-sudo                                                     date "+FECHA: %D%nHORA: %T"     
+date "+FECHA: %D%nHORA: %T"   
 msg -bar                                                                                                
 echo -e "  \033[100m     DIARIO DEL DESARROLADOR " 
 msg -bar 
