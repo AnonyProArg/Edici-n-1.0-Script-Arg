1189a2549d06cb94c3818dc99c595ff0064d320f
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2021-06-02 16:29:01 -0300

    Update hora.sh

--- Modulos/hora.sh
+++ Modulos/hora.sh
@@ -43,6 +43,7 @@ echo -e "${cor[3]}          PON LA OPCION DONDE ESTE TU PAIS"
 msg -bar
 echo -e "${cor[4]} 2).-\033[1;37m ARGENTINA, BRASIL, CHILE, URUGUAY "
 echo -e "${cor[4]} 3).-\033[1;37m MÉXICO, PANAMÁ, LIMA, BOGOTÁ, ETC"
+echo   " 0) INICIO"
 msg -bar
 echo -ne     "\033[1;37m ESCOJA SU OPCION: "
 read opcao
