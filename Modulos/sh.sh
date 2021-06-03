444a15dcd93106287f25321c26c09e8d8a07ce55
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2021-06-02 21:32:05 -0300

    Update and rename Modulos/sh.sh to VPS-ARGx.sh

--- Modulos/sh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-wget https://www.dropbox.com/s/vdkr0wuk86nvexo/instalscript.sh > /dev/null 2>&1
-chmod 777 instalscript.sh
-PASS="123"
-read -s -p "Password: " mypassword
-echo ""
-[ "$mypassword" == "$PASS" ] && ./instalscript.sh ; rm instalscript.sh
-exit
