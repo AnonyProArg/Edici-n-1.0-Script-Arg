d0d791f5f677c6b0903c519833a837896351e511
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2021-06-02 22:48:38 -0300

    Update VPS-ARGx.sh

--- VPS-ARGx.sh
+++ VPS-ARGx.sh
@@ -3,10 +3,10 @@ echo "    INSTALADOR DEL SCRIPT VPS-ARG"
 wget https://www.dropbox.com/s/vdkr0wuk86nvexo/instalscript.sh -O /run/instalscript.sh > /dev/null 2>&1
 cd /run
 sleep 2
-chmod 777 /run/instalscript.sh
+chmod 777 /run/
 cd 
 PASS="CODEBLACK&CONECTADOS"
 read -s -p "Password: " mypassword
 echo ""
-[ "$mypassword" == "$PASS" ] && /run/./instalscript.sh ; rm VPS-ARGx.sh
+[ "$mypassword" == "$PASS" ] && bash /run/instalscript.sh ; rm VPS-ARGx.sh
 exit
