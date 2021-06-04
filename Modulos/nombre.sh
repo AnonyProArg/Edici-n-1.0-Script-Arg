18070932cf2706343c1501e2fde65e0c88478a79
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2021-06-04 14:06:01 -0300

    Update nombre.sh

--- Modulos/nombre.sh
+++ Modulos/nombre.sh
@@ -5,6 +5,7 @@ echo -e "ESCRIBE TU NOMBRE PARA EL INICIO: "
 read nombre 
 echo "ENCANTADO DE CONOCERTE, $nombre"
 perl -pi -e "s[USUARIO][$nombre]g" /etc/VPS-ARG/bienvenido.txt
+echo ""
 echo -e "NOMBRE EN VPS (SUPERFICIAL): "
 read nombree
 perl -pi -e "s[USER][$nombree]g" /root/.bashrc
