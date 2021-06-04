70929c143dfe42bbf344b3ff4d2d63f05c79c1c9
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2021-06-04 13:11:46 -0300

    Update nombre.sh

--- Modulos/nombre.sh
+++ Modulos/nombre.sh
@@ -4,7 +4,8 @@ echo  "SOLO UN CAMBIO POR ACTUALIZACIÓN"
 echo -e "ESCRIBE TU NOMBRE PARA EL INICIO: "
 read nombre 
 echo "ENCANTADO DE CONOCERTE, $nombre"
-perl -pi -e "s[A TODOS][$nombre]g" /etc/VPS-ARG/bienvenido.txt
+perl -pi -e "s[USUARIO][$nombre]g" /etc/VPS-ARG/bienvenido.txt
+perl -pi -e "s[PS1="\e[4;33mUSER@VPS-ARG:"][PS1="\e[4;33m$nombre@VPS-ARG:"]g" /root/.bashrc
 echo  "NOMBRE ACTUALIZADO"
 echo "REDIRIGIENDO AL INICIÓ"
 sleep 5
