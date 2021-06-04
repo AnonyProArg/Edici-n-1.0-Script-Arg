68e1294ccb033131b5c896b4117a9a143e83af75
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2021-06-04 19:22:37 -0300

    Update hora.sh

--- Modulos/hora.sh
+++ Modulos/hora.sh
@@ -6,8 +6,13 @@ echo  "SOLO UN CAMBIO POR ACTUALIZACIÓN"
 echo -e "ESCRIBE TU NOMBRE PARA EL INICIO: "
 read nombre 
 echo "ENCANTADO DE CONOCERTE, $nombre"
-perl -pi -e "s[USUARIO  ][$nombre]g" /etc/VPS-ARG/bienvenido.txt
-echo  "NOMBRE ACTUALIZADO"
+perl -pi -e "s[USUARIO][$nombre]g" /etc/VPS-ARG/bienvenido.txt
+echo ""
+echo -e "NOMBRE EN VPS (SUPERFICIAL): "
+read nombree
+perl -pi -e "s[USER][$nombree]g" /root/.bashrc
+echo "SE RECOMIENDA SALIR Y ENTRAR NUEVAMENTE"
+echo  "NOMBRES ACTUALIZADOS"
 echo "REDIRIGIENDO AL INICIÓ"
 sleep 5
 VPS-ARG
