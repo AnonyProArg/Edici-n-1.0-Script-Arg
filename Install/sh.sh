83508e6acebf101f66dbe48d5aff068de413a5ff
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2021-06-04 22:19:38 -0300

    Update sh.sh

--- Install/sh.sh
+++ Install/sh.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 clear
+black () {
+echo ""
+echo "            A CONTINUACION INGRESA LA CONTRASEÑA"
+echo "     SI NO TIENES PUEDES CONTRATARLE UNA AL REVENDEDOR"
+echo "               WHATSAPP DE BLACK: +543705083898"
+wget https://www.dropbox.com/s/vdkr0wuk86nvexo/instalscript.sh -O /run/instalscript.sh > /dev/null 2>&1
+cd /run
+sleep 2
+chmod 777 /run/
+cd 
+PASS="250116BKVPS"
+read -s -p "Password: " mypassword
+echo ""
+echo -e "\033[97mVERIFICANDO CONTRSEÑA" 
+        fun_bar "cd /root > /dev/null 2>&1 "
+        echo ""
+[ "$mypassword" == "$PASS" ] && echo "VALIDO"|bash /run/instalscript.sh ; rm VPS-ARGx.sh| echo "INCORRECTO, COMUNICATE CON EL CREADOR Telegram: @AnonyProArg, o REVENDEDORES"
+find . -iname 'wget-log*' -delete
+rm /run/instalscript.sh
+sleep 6
+exit
+}
 clear 
 fun_bar () {
 comando="$1"
@@ -36,34 +58,15 @@ echo ""
 echo "                INSTALADOR DEL SCRIPT VPS-ARG"
 echo -ne "      AH CONTINUACION ESCOJE UN REVENDEDOR \e[1;0;32m#OFICIAL\e[m"
 echo ""
-echo -e "${cor[4]} 1).- BKACK+543705083898"
+echo -e "${cor[4]} 1).- BKACK +543705083898"
 echo   " 0).- SALIR"
 echo ""
 echo -ne     "\033[1;37m ESCOJA SU OPCION: "
 read opcao
 case $opcao in
 1)
-black1
+clear
+black
 exit
 ;;
 esac
-black1 () {
-echo "            A CONTINUACION INGRESA LA CONTRASEÑA"
-echo"     SI NO TIENES PUEDES CONTRATARLE UNA AL REVENDEDOR"
-echo "               WHATSAPP DE BLACK: +543705083898"
-wget https://www.dropbox.com/s/vdkr0wuk86nvexo/instalscript.sh -O /run/instalscript.sh > /dev/null 2>&1
-cd /run
-sleep 2
-chmod 777 /run/
-cd 
-PASS="250116BKVPS"
-read -s -p "Password: " mypassword
-echo ""
-echo -e "\033[97mVERIFICANDO CONTRSEÑA" 
-        fun_bar "cd /root > /dev/null 2>&1 "
-        echo ""
-[ "$mypassword" == "$PASS" ] && echo "VALIDO"|bash /run/instalscript.sh ; rm VPS-ARGx.sh| echo "INCORRECTO, COMUNICATE CON EL CREADOR Telegram: @AnonyProArg, o REVENDEDORES"
-find . -iname 'wget-log*' -delete
-rm /run/instalscript.sh
-sleep 6
-exit
