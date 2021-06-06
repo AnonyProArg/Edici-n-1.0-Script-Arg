09b7a9cb2d2a73d91712cd24f6c840382ab5407a
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2021-06-06 19:23:14 -0300

    Update and rename multissl.sh to ssl1.sh

--- Modulos/ssl1.sh
+++ Modulos/ssl1.sh
@@ -1,27 +1,7 @@
 #!/bin/bash
-#    .---------- constant part!
-#    vvvv vvvv-- the code from above
-RED='\033[0;31m'
-NC='\033[0m' # No Color
-clear
-clear
-printf "${NC}+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
-echo -e ""
-printf "${RED}INSTALADOR MULTISSL By AnonyProArg 🇦🇷\n"
-echo -e ""
-printf "${NC}+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
-echo -e ""
-printf "${RED}DEBES INSTALAR ESTE SCRIPT DESDE UNA VPS TOTALMENTE LIMPIA SIN USAR LOS PUERTOS 443 80 22"
-echo -e ""
-printf "${NC}+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
-echo -e ""
 printf "${RED}INSTANDO PYTHON DIRECT MODIFICADO..."
 echo -e ""
-printf "${NC}+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
 screen -dmS Pydirect python /etc/VPS-ARG/protocolos/PDirect.py 80
-printf "${RED}Se instalará SSL en puerto 443 redirecionado a python 80"
-echo -e ""
-printf "${NC}+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
 echo -e ""
 printf "${RED}Instalando SSL"
 echo -e ""
@@ -39,11 +19,9 @@ mv stunnel.pem /etc/stunnel/
 ######-------
 sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
 service stunnel4 restart > /dev/null 2>&1
-printf "${NC}+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
-echo -e ""
-printf "${RED}INSTALADO STUNNEL4 CON EXITO"
 echo -e ""
 printf "${NC}+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
+echo -e ""
 rm -rf /etc/ger-frm/stunnel.crt > /dev/null 2>&1
 rm -rf /etc/ger-frm/stunnel.key > /dev/null 2>&1
 rm -rf /root/stunnel.crt > /dev/null 2>&1
