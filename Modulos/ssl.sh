264d0313b0f9e3748f7b5e2625318a8087ccd3c2
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2021-08-01 00:18:42 -0300

    Update ssl.sh

--- Modulos/ssl.sh
+++ Modulos/ssl.sh
@@ -386,41 +386,6 @@ def main(host=LISTENING_ADDR, port=LISTENING_PORT):
 if __name__ == '__main__':
     main()
 
-echo -e ""
-echo -e "\033[1;33m   Ahora Que Puerto sera SSL"
-msg -bar
-    read -p " Puerto SSL: " sslpoti
-echo -e ""
-screen -dmS Pydirect python /etc/VPS-ARG/protocolos/PDirect.py $pythonport
-echo -e ""
-printf "${RED}Instalando SSL"
-echo -e ""
-printf "${NC}+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
-apt-get install stunnel4 -y
-apt-get install stunnel4 -y > /dev/null 2>&1
-echo -e "client = no\n[SSL]\ncert = /etc/stunnel/stunnel.pem\naccept = $sslpoti\nconnect = 127.0.0.1:80" > /etc/stunnel/stunnel.conf
-openssl genrsa -out stunnel.key 2048 > /dev/null 2>&1
-
-(echo "US" ; echo "California" ; echo "San Francisco" ; echo "Cloudflare, inc." ; echo "" ; echo "sni.cloudflaressl.com" ; echo "" )|openssl req -new -key stunnel.key -x509 -days 1000 -out stunnel.crt > /dev/null 2>&1
-
-cat stunnel.crt stunnel.key > stunnel.pem 
-
-mv stunnel.pem /etc/stunnel/
-######-------
-sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
-service stunnel4 restart > /dev/null 2>&1
-echo -e ""
-printf "${NC}+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
-echo -e ""
-rm -rf /etc/ger-frm/stunnel.crt > /dev/null 2>&1
-rm -rf /etc/ger-frm/stunnel.key > /dev/null 2>&1
-rm -rf /root/stunnel.crt > /dev/null 2>&1
-rm -rf /root/stunnel.key > /dev/null 2>&1
-printf "${NC}+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
-echo -e ""
-printf "${RED}INSTALACION FINALIZADA"
-return 0
-}
 
 PYTHON
 ) > $HOME/proxy.log
