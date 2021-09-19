78720e4ef527858d7b106a65829b402dfafa7226
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2021-09-19 18:25:32 -0300

    Update ModPublicidadScrips.sh

--- ModPublicidadScrips.sh
+++ ModPublicidadScrips.sh
@@ -1,3 +1,13 @@
+echo -e "Texto #0 (tu texto): "
+
+read Textooo
+
+echo "Se a modificado a: $Texto"
+
+perl -pi -e "s[Texto0][$Textooo]g" /etc/dropbear/banner
+
+echo ""
+
 echo -e "Texto #1 (Nombre del Vendedor): "
 
 read Texto
@@ -7,6 +17,7 @@ echo "Se a modificado a: $Texto"
 perl -pi -e "s[Texto1][$Texto]g" /etc/dropbear/banner
 
 echo ""
+
 echo -e "Texto #2 (Nombre del Script): "
 
 read Textoo
