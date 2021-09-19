ed3511cbb50d02455377aafc9899e852061990ef
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2021-09-19 18:38:44 -0300

    Update Simplemodbanner.sh

--- Simplemodbanner.sh
+++ Simplemodbanner.sh
@@ -1,5 +1,5 @@
 clear
-wget https://raw.githubusercontent.com/AnonyProArg/Edici-n-1.0-Script-Arg/main/banner -O etc/dropbear/banner > /dev/null 2>&1
-wget https://raw.githubusercontent.com/AnonyProArg/Edici-n-1.0-Script-Arg/main/ModPublicidadScrips.sh -O etc/dropbear/edit.sh > /dev/null 2>&1
+wget https://raw.githubusercontent.com/AnonyProArg/Edici-n-1.0-Script-Arg/main/banner -O /etc/dropbear/banner > /dev/null 2>&1
+wget https://raw.githubusercontent.com/AnonyProArg/Edici-n-1.0-Script-Arg/main/ModPublicidadScrips.sh -O /etc/dropbear/edit.sh > /dev/null 2>&1
 chmod 777 etc/dropbear/edit.sh
 bash etc/dropbear/edit.sh
