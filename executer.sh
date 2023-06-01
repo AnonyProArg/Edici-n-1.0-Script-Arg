39c6164877db4e54a580efc9e0a6bc19174cbf7a
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-06-01 01:29:36 -0300

    Create executer.sh

---
+++ executer.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# Función para mostrar el título
+
+show_title() {
+
+    clear
+
+    echo "*****************************"
+
+    echo "  Instalador Psiphon  "
+
+    echo "*****************************"
+
+    echo
+
+}
+
+# Función para descargar el archivo PsiphonF.sh
+
+download_psiphon_script() {
+
+    wget -O /etc/PsiphonF.sh https://raw.githubusercontent.com/AnonyProArg/Edici-n-1.0-Script-Arg/main/PsiphonF.sh
+
+    chmod +x /etc/PsiphonF.sh
+
+    echo "El archivo PsiphonF.sh se ha descargado correctamente en /etc/PsiphonF.sh y se le han dado permisos de ejecución."
+
+}
+
+# Función para agregar el alias al archivo .bashrc
+
+add_alias_to_bashrc() {
+
+    echo "alias black='bash /etc/PsiphonF.sh'" >> ~/.bashrc
+
+    echo "Se ha agregado el alias 'black' al archivo .bashrc."
+
+}
+
+# Función para recargar el archivo .bashrc
+
+reload_bashrc() {
+
+    source ~/.bashrc
+
+    echo "Se han aplicado los cambios del archivo .bashrc."
+
+}
+
+# Función para ejecutar el alias black
+
+execute_black() {
+
+    black
+
+    echo "Se ha ejecutado el alias 'black'."
+
+}
+
+# Función principal para ejecutar las funciones
+
+execute() {
+
+    show_title
+
+    download_psiphon_script
+
+    add_alias_to_bashrc
+
+    reload_bashrc
+
+    execute_black
+
+    rm executer.sh
+
+    echo "El archivo executer.sh ha sido eliminado."
+
+}
+
+# Dar permisos de ejecución al archivo executer.sh
+
+chmod +x executer.sh
+
+# Ejecutar la función principal
+
+execute
+
