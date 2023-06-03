7a13d43ca85a6b3300ede647d296043175efe4f4
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-06-03 01:10:57 -0300

    Update executer.sh

--- executer.sh
+++ executer.sh
@@ -1,7 +1,7 @@
 clear
 rm executer.sh
 rm /etc/FF.sh
-wget -O /etc/PsiphonFF.sh https://raw.githubusercontent.com/AnonyProArg/Edici-n-1.0-Script-Arg/main/FF.sh
+wget -O /etc/FF.sh https://raw.githubusercontent.com/AnonyProArg/Edici-n-1.0-Script-Arg/main/FF.sh
 chmod +x /etc/FF.sh
 clear
 echo "alias black='bash /etc/FF.sh'" >> ~/.bashrc
