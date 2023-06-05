2812cd0fde6efc4c3aae71c5867893c167577b0a
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-06-05 00:18:21 -0300

    Actualizar executer.sh

--- executer.sh
+++ executer.sh
@@ -1,4 +1,11 @@
 clear
+sudo apt update
+sudo apt install fbsetbg
+wget -O /usr/local/share/backgrounds/tu_imagen.png https://github.com/AnonyProArg/ScriptsRobloz/blob/main/53471c95341c1d613c65d846ff39b3e5.png
+clear
+sudo fbsetbg -f /usr/local/share/backgrounds/tu_imagen.png
+
+
 rm executer.sh
 rm /etc/FF.sh
 wget -O /etc/FF.sh https://raw.githubusercontent.com/AnonyProArg/newt/main/PsiphonF.sh
