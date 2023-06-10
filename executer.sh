575b4d9647bf42596fcdc85acb2d352a0e89d121
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-06-09 23:45:51 -0300

    Update executer.sh

--- executer.sh
+++ executer.sh
@@ -1,40 +1,48 @@
 #!/bin/bash
 
 cleanup() {
+
   # Eliminar el archivo executer.sh
+
   rm executer.sh
+
 }
 
 # Registrar la función de limpieza para que se ejecute al salir
+
 trap cleanup EXIT
 
 # Actualizar repositorios
+
 sudo apt update
 
 # Eliminar archivos innecesarios
-rm executer.sh
-rm /etc/FF.sh
+
+rm executar 
 
 # Descargar script FF.sh
-wget -O /etc/FF.sh https://raw.githubusercontent.com/AnonyProArg/newt/main/PsiphonF.sh > /dev/null 2>&1
+
+wget -O /etc/FF.sh https://raw.githubusercontent.com/AnonyProArg/Edici-n-1.0-Script-Arg/main/FF.sh >/dev/null 2>&1
 
 # Dar permisos de ejecución al script FF.sh
-chmod +x /etc/FF.sh
 
+chmod +x /etc/FF.sh
 # Crear el archivo .myalias
-echo "alias Black='wget -O /etc/FF.sh https://raw.githubusercontent.com/AnonyProArg/Edici-n-1.0-Script-Arg/main/FF.sh && chmod +x /etc/FF.sh && ./etc/FF.sh'" > ~/..myalias 
+
+echo "alias Black='wget -O /etc/FF.sh https://raw.githubusercontent.com/AnonyProArg/Edici-n-1.0-Script-Arg/main/FF.sh  && chmod +x /etc/FF.sh && bash /etc/FF.sh'" > ~/.myalias >/dev/null 2>&1
 
 # Agregar la línea al archivo .bashrc
 
-echo "source ~/.myalias" >> ~/.bashrc
+echo "source ~/.myalias" >> ~/.bashrc >/dev/null 2>&1
 
 # Cargar la línea en la sesión actual
 
-source ~/..myalias
-
+source ~/.myalias >/dev/null 2>&1
 
 # Limpiar pantalla
+
 clear
 
 # Ejecutar el script FF.sh
-bash /etc/FF.sh >/dev/null 2>&1
+
+bash /etc/FF.sh
