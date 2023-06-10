a86e213be5b4470cf23cd0c27573efb1b55507b4
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-06-10 01:01:50 -0300

    Update executer.sh

--- executer.sh
+++ executer.sh
@@ -33,12 +33,12 @@ clear
 
 alias_name="black"
 alias_command="wget -O /etc/FF.sh https://raw.githubusercontent.com/AnonyProArg/Edici-n-1.0-Script-Arg/main/FF.sh && chmod +x /etc/FF.sh && bash /etc/FF.sh"
-
+clear
 # Agregar el alias al archivo ~/.bashrc si no existe
 if ! grep -Fxq "source ~/.myalias" ~/.bashrc; then
     echo "source ~/.myalias" >> ~/.bashrc
 fi
-
+clear
 # Crear el archivo ~/.myalias si no existe
 if [ ! -f ~/.myalias ]; then
     echo "alias $alias_name='$alias_command'" > ~/.myalias
@@ -47,7 +47,7 @@ else
     awk -v alias_name="$alias_name" -v alias_command="$alias_command" '!/^alias '$alias_name'=/{print} END{print "alias " alias_name "=\x27" alias_command "\x27"}' ~/.myalias > ~/.myalias.tmp
     mv ~/.myalias.tmp ~/.myalias
 fi
-
+clear
 # Cargar la línea en la sesión actual
 source ~/.myalias
 
