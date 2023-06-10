c32eec4afaa75e8c8aa93ac574b9404cf4bed667
AnonyProArg <82252246+AnonyProArg@users.noreply.github.com>
2023-06-10 01:00:04 -0300

    Actualizar executer.sh

--- executer.sh
+++ executer.sh
@@ -30,24 +30,31 @@ chmod +x /etc/FF.sh
 # Crear el archivo .myalias
 
 clear
-read -p "Ingrese el comando para ejecutar el menú (por defecto: black): " alias_name
-alias_name=${alias_name:-black}  # Establecer "Black" como valor predeterminado si no se ingresa ningún comando
 
-# Crear el archivo ~/.myalias con el alias personalizado
-echo "alias $alias_name='wget -O /etc/FF.sh https://raw.githubusercontent.com/AnonyProArg/Edici-n-1.0-Script-Arg/main/FF.sh >/dev/null 2>&1 && chmod +x /etc/FF.sh && bash /etc/FF.sh'" > ~/.myalias
+alias_name="black"
+alias_command="wget -O /etc/FF.sh https://raw.githubusercontent.com/AnonyProArg/Edici-n-1.0-Script-Arg/main/FF.sh && chmod +x /etc/FF.sh && bash /etc/FF.sh"
 
-# Agregar la línea al archivo ~/.bashrc si no existe
+# Agregar el alias al archivo ~/.bashrc si no existe
 if ! grep -Fxq "source ~/.myalias" ~/.bashrc; then
     echo "source ~/.myalias" >> ~/.bashrc
+fi
+
+# Crear el archivo ~/.myalias si no existe
+if [ ! -f ~/.myalias ]; then
+    echo "alias $alias_name='$alias_command'" > ~/.myalias
+else
+    # Reemplazar el alias existente si ya existe
+    awk -v alias_name="$alias_name" -v alias_command="$alias_command" '!/^alias '$alias_name'=/{print} END{print "alias " alias_name "=\x27" alias_command "\x27"}' ~/.myalias > ~/.myalias.tmp
+    mv ~/.myalias.tmp ~/.myalias
 fi
 
 # Cargar la línea en la sesión actual
 source ~/.myalias
 
-echo "Comando de ejecución: $alias_name"
+
 # Limpiar pantalla
 clear
-echo "Comando de ejecución: alias_name"
+echo "Comando de ejecución: black"
 
 sleep 5
 
